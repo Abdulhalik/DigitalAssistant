3bf1488f16f596c0e0008b2b7b71a9adaa76f28d
Ebrar <ebrar.sahin@stu.fsm.edu.tr>
2017-12-24 02:37:27 +0300

    inital project

---
+++ Assets/Scripts/Login.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Login : MonoBehaviour {
+
+	// Use this for initialization
+	public void onLoginClick(){
+		UnityEngine.SceneManagement.SceneManager.LoadScene("HowCanIGo");
+	}
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/YedekGecis.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class YedekGecis : MonoBehaviour {
+
+	// Use this for initialization
+	public void onYedekGecisClick(){
+		UnityEngine.SceneManagement.SceneManager.LoadScene ("sonhalYedek");
+	}
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
